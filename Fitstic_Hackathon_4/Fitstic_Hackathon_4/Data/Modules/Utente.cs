405ef2a7c8e44798821a2fd1f06128c24b04686a
Edoardo Zanotti <edoardo.zanotti2@studio.unibo.it>
2020-06-02 13:54:16 +0200

    Inserimento pagine login +signup come scaffolding elements

---
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Data/Modules/Utente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prova1.Data.Modules
+{
+    public class Utente
+    {        
+        public int UserId { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Telefono { get; set; }
+        public string Token { get; set; }
+
+    }
+}
--- Fitstic_Hackathon_4/Fitstic_Hackathon_4/Startup.cs
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Startup.cs
@@ -27,22 +27,17 @@ namespace Fitstic_Hackathon_4
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options =>
+                                                      options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -53,7 +48,6 @@ namespace Fitstic_Hackathon_4
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -67,10 +61,9 @@ namespace Fitstic_Hackathon_4
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapRazorPages();
             });
         }
     }
 }
+
