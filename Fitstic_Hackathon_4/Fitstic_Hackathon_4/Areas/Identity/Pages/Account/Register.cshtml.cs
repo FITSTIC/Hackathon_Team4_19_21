5ee2e9aa697d4d3ce9c71755d0f981e83236259b
Edoardo Zanotti <edoardo.zanotti2@studio.unibo.it>
2020-06-04 16:26:48 +0200

    sistemazione form registrazione

--- Fitstic_Hackathon_4/Fitstic_Hackathon_4/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,27 +45,11 @@ namespace Fitstic_Hackathon_4.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
-            [DataType(DataType.Text)]
-            [Display(Name = "Nome")]
-            public string Nome { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
-            [DataType(DataType.Text)]
-            [Display(Name = "Cognome")]
-            public string Cognome { get; set; }
+          
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
-            [DataType(DataType.PhoneNumber)]
-            [Display(Name = "Phone")]
-            public string Phone { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -73,10 +57,7 @@ namespace Fitstic_Hackathon_4.Areas.Identity.Pages.Account
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+        
         }
 
         public async Task OnGetAsync(string returnUrl = null)
