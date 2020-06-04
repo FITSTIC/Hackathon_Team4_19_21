a053b0a7e1542d515f150422351817442c0ece17
Edoardo Zanotti <edoardo.zanotti2@studio.unibo.it>
2020-06-04 16:50:49 +0200

    inizio db + classe Personale

--- Fitstic_Hackathon_4/Fitstic_Hackathon_4/Data/ApplicationDbContext.cs
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using prova1.Data;
 
 namespace Fitstic_Hackathon_4.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Personale> Operatori { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
