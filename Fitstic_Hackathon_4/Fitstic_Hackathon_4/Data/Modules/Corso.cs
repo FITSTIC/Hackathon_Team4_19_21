23bd4cbe69e160d4d73d112b164a69fbeeb41a90
Edoardo Zanotti <edoardo.zanotti2@studio.unibo.it>
2020-06-13 11:50:57 +0200

    pagina + form Modulo e fix classi Corso e Modulo

--- Fitstic_Hackathon_4/Fitstic_Hackathon_4/Data/Modules/Corso.cs
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Data/Modules/Corso.cs
@@ -15,7 +15,8 @@ namespace Fitstic_Hackathon_4.Data
         public DateTime DataInizio { get; set; }
         public DateTime DataFine { get; set; }
         public string Sede { get; set; }
-        public int OrganizzatoreId { get; set; }
+        public int? OrganizzatoreId { get; set; }
+        public Organizzatore Organizzatore { get; set; }
         public List<Modulo> ListaModuli { get; set; }
         public List<Partecipante> ListaPartecipanti { get; set; }
 
--- Fitstic_Hackathon_4/Fitstic_Hackathon_4/Migrations/20200613083343_RelDb_TabCorso.Designer.cs
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/Migrations/20200613083343_RelDb_TabCorso.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Fitstic_Hackathon_4.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fitstic_Hackathon_4.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200613083343_RelDb_TabCorso")]
+    partial class RelDb_TabCorso
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
