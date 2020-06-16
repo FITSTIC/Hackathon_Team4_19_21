9851768c150f42d15403e66843baf1b1df1b8166
Edoardo Zanotti <edoardo.zanotti2@studio.unibo.it>
2020-06-16 16:09:22 +0200

    fix foreign key + file js + fix link tag + fix cards

---
+++ Fitstic_Hackathon_4/Fitstic_Hackathon_4/wwwroot/js/ActivateSelect2.js
@@ -0,0 +1,6 @@
+﻿function activateSelect2() {
+    $('.js-select2-multiple').select2({
+        placeholder: 'Seleziona gli studenti da iscrivere'
+    });
+}
+                                       
